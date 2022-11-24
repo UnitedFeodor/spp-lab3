5508cb0734dcf3ffb5f9ffcf82b6853ce0dfee61
UnitedFeodor <86407851+UnitedFeodor@users.noreply.github.com>
2022-11-24 19:13:51 +0300

    added wpf gui

---
+++ spp-lab3/Model/ModelFileTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace directory_scanner.wpf.Model
+{
+    public class ModelFileTree
+    {
+        public bool IsDirectory { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public ulong Length { get; set; } //file size
+        public double LengthPercentage { get; set; }
+        public List<ModelFileTree> Children { get; set; }
+        public string Icon { get; set; }
+
+        public ModelFileTree()
+        {
+
+        }
+
+        public ModelFileTree(FileTree tree)
+        {
+            Name = tree.Name;
+            Length = tree.Length;
+            LengthPercentage = tree.LengthPercentage;
+            Icon = tree.IsDirectory ? "Icons/folder.png" : "Icons/file.png";
+            
+            Children = tree.Children?.Select(c => new ModelFileTree(c)).ToList();
+        }
+       
+        
+
+    }
+}
---
+++ spp-lab3/ViewModel/AppViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using directory_scanner.wpf.Model;
+using System.Windows.Forms;
+using Presentation.Command;
+using directory_scanner.directory_scanner;
+
+namespace directory_scanner.wpf.ViewModel
+{
+
+    public class AppViewModel : INotifyPropertyChanged
+    {
+        public RelayCommand SetDirectoryCommand { get; set; }
+        public RelayCommand StartScanningCommand { get; set; }
+        public RelayCommand StopScanningCommand { get; set; }
+
+        private DirectoryScanner _directoryScanner;
+
+        private string? _path;
+        public string? Path
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ushort _maxThreadCount = 20;
+
+        public ushort MaxThreadCount
+        {
+            get => _maxThreadCount;
+            set
+            {
+                _maxThreadCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ModelFileTree _tree;
+
+        public ModelFileTree Tree
+        {
+            get => _tree;
+            set
+            {
+                _tree = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isScanning;
+        public bool IsScanning
+        {
+            get => _isScanning;
+            set
+            {
+                _isScanning = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AppViewModel()
+        {
+            SetDirectoryCommand = new RelayCommand(_ =>
+            {
+                using var folderBrowserDialog = new FolderBrowserDialog();
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Path = folderBrowserDialog.SelectedPath;
+                }
+            });
+
+            StartScanningCommand = new RelayCommand(_ =>
+            {
+                IsScanning = true;
+                Task.Run(() =>
+                {
+                    _directoryScanner = new DirectoryScanner(MaxThreadCount);
+                    _directoryScanner.Start(Path);
+                    var directoryTree = _directoryScanner.GetResult();
+                    Tree = new ModelFileTree();
+                    Tree.Children = new List<ModelFileTree>() { new(directoryTree) };
+                    IsScanning = false;
+                });
+            }, _ => Path != null && !IsScanning);
+
+            StopScanningCommand = new RelayCommand(_ =>
+            {
+                var directoryTree = _directoryScanner.Stop();
+                IsScanning = false;
+                Tree = new ModelFileTree()
+                {
+                    Children = new List<ModelFileTree>()
+                    {
+                    new(directoryTree)
+                    }
+                };
+            }, _ => IsScanning);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
